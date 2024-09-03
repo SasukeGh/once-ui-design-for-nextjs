f018d6c0df3e6fdf2b0393534ef01ac3011cf4be
SasukeGh <162058638+SasukeGh@users.noreply.github.com>
2024-09-03 09:26:06 +0000

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@
 
 import React from 'react';
 
-import { Heading, Text, Flex, Button, Grid, Icon, InlineCode, Logo, Background } from '@/once-ui/components';
+import { Heading, Text, Flex, Button, Grid, Icon, Logo, Background } from '@/once-ui/components';
 import Link from 'next/link';
 
 export default function Home() {
