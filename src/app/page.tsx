f0091e9680b55d3949137dd2f73aacde5532a501
SasukeGh <162058638+SasukeGh@users.noreply.github.com>
2024-09-03 11:34:05 +0000

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 "use client";
-import Analytics from '@vercel/analytics';
+
+import { Analytics } from '@vercel/analytics/react'; // Updated import statement
 import React from 'react';
 
 import { Heading, Text, Flex, Button, Grid, InlineCode, Icon, Logo, Background } from '@/once-ui/components';
@@ -10,7 +11,7 @@ export default function Home() {
 		{
 			href: "https://whatsapp.com/channel/0029Va8aNxzJENy5Obt7Tz0t",
 			title: "My WhatsApp Channel",
-			description: "Join ChillHub,my channel for fun,memes,anime,and tech",
+			description: "Join ChillHub, my channel for fun, memes, anime, and tech",
 		},
 		{
 			href: "https://wa.me/233257789272/?text=Hi,+there+Sasuke+Uchiha",
@@ -25,112 +26,115 @@ export default function Home() {
 	];
 
 	return (
-		<Flex
-			fillWidth paddingTop="l" paddingX="l"
-			direction="column" alignItems="center" flex={1}>
-			<Background
-				dots={false}/>
+		<React.Fragment>
 			<Flex
-				position="relative"
-				as="section" overflow="hidden"
-				fillWidth minHeight="0" maxWidth={64}
+				fillWidth paddingTop="l" paddingX="l"
 				direction="column" alignItems="center" flex={1}>
+				<Background
+					dots={false} />
 				<Flex
-					as="main"
-					direction="column" justifyContent="center"
-					fillWidth fillHeight padding="l" gap="l">
+					position="relative"
+					as="section" overflow="hidden"
+					fillWidth minHeight="0" maxWidth={64}
+					direction="column" alignItems="center" flex={1}>
 					<Flex
-						mobileDirection="column"
-						fillWidth gap="24">
+						as="main"
+						direction="column" justifyContent="center"
+						fillWidth fillHeight padding="l" gap="l">
 						<Flex
-							position="relative"
-							fillWidth paddingTop="56" paddingX="xl">
-							<Logo size="xl" icon={false} style={{zIndex: '1'}}/>
+							mobileDirection="column"
+							fillWidth gap="24">
+							<Flex
+								position="relative"
+								fillWidth paddingTop="56" paddingX="xl">
+								<Logo size="xl" icon={false} style={{ zIndex: '1' }} />
+							</Flex>
+							<Flex
+								position="relative"
+								fillWidth gap="24" marginBottom="104"
+								direction="column">
+								<InlineCode
+									className="shadow-m"
+									style={{
+										width: 'fit-content',
+										padding: 'var(--static-space-8) var(--static-space-16)',
+										backdropFilter: 'blur(var(--static-space-1))'
+									}}>
+									Welcome to the official website of<span className="brand-on-background-medium"> SasukeTech</span>
+								</InlineCode>
+								<Heading
+									variant="display-strong-s">
+									Sasuke Uchiha.<br />Web developer.<br /><br />Contact me for any projects at a fee
+								</Heading>
+								<Button
+									href="https://conectarorder.vercel.app"
+									suffixIcon="chevronRight"
+									variant="secondary">
+									My First Main Project
+								</Button>
+							</Flex>
 						</Flex>
-						<Flex
-							position="relative"
-							fillWidth gap="24" marginBottom="104"
-							direction="column">
-							<InlineCode
-								className="shadow-m"
-								style={{
-									width: 'fit-content',
-									padding: 'var(--static-space-8) var(--static-space-16)',
-									backdropFilter: 'blur(var(--static-space-1))'}}>
-								Welcome to the official website of<span className="brand-on-background-medium"> SasukeTech</span>
-							</InlineCode>
-							<Heading
-								variant="display-strong-s">
-								Sasuke Uchiha.<br/>Web developer.<br/> <br/>Contact me for any projects at a fee
-							</Heading>
-							<Button
-								href="https://conectarorder.vercel.app"
-								suffixIcon="chevronRight"
-								variant="secondary">
-								My First Main Project
-							</Button>
-						</Flex>
-					</Flex>
-					<Grid
-						radius="l"
-						border="neutral-medium"
-						borderStyle="solid-1"
-						columns="repeat(3, 1fr)"
-						tabletColumns="1col"
-						mobileColumns="1col"
-						fillWidth>
-						{links.map((link) => (
-							<Link
-								target="_blank"
-								style={{ padding: 'var(--responsive-space-l)' }}
-								key={link.href}
-								href={link.href}>
-								<Flex
-									fillWidth paddingY="8" gap="8"
-									direction="column">
+						<Grid
+							radius="l"
+							border="neutral-medium"
+							borderStyle="solid-1"
+							columns="repeat(3, 1fr)"
+							tabletColumns="1col"
+							mobileColumns="1col"
+							fillWidth>
+							{links.map((link) => (
+								<Link
+									target="_blank"
+									style={{ padding: 'var(--responsive-space-l)' }}
+									key={link.href}
+									href={link.href}>
 									<Flex
-										fillWidth gap="12"
-										alignItems="center">
+										fillWidth paddingY="8" gap="8"
+										direction="column">
+										<Flex
+											fillWidth gap="12"
+											alignItems="center">
+											<Text
+												variant="body-strong-m" onBackground="neutral-strong">
+												{link.title}
+											</Text>
+											<Icon size="s" name="arrowUpRight" />
+										</Flex>
 										<Text
-											variant="body-strong-m" onBackground="neutral-strong">
-											{link.title}
+											variant="body-default-s" onBackground="neutral-weak">
+											{link.description}
 										</Text>
-										<Icon size="s" name="arrowUpRight" />
 									</Flex>
-									<Text
-										variant="body-default-s" onBackground="neutral-weak">
-										{link.description}
-									</Text>
-								</Flex>
-							</Link>
-						))}
-					</Grid>
+								</Link>
+							))}
+						</Grid>
+					</Flex>
 				</Flex>
-			</Flex>
-			<Flex
-				as="footer"
-				position="relative"
-				fillWidth paddingX="l" paddingY="m"
-				justifyContent="space-between">
-				<Text
-					variant="body-default-s" onBackground="neutral-weak">
-					© 2024 SasukeTech, <Link href=" https://raw.githubusercontent.com/SasukeGh/once-ui-design-for-nextjs/main/LICENSE?token=GHSAT0AAAAAACWGN46IT2W2G2F7DGWDK2LQZWW2Q6Q">MIT License</Link>
-				</Text>
 				<Flex
-					gap="12">
-					<Button
-						href="https://github.com/SasukeGh"
-						prefixIcon="github" size="s" variant="tertiary">
-						GitHub
-					</Button>
-					<Button
-						href="https://whatsapp.com/channel/0029Va8aNxzJENy5Obt7Tz0t"
-						prefixIcon="discord" size="s" variant="tertiary">
-						Whatsapp Channel
-					</Button>
+					as="footer"
+					position="relative"
+					fillWidth paddingX="l" paddingY="m"
+					justifyContent="space-between">
+					<Text
+						variant="body-default-s" onBackground="neutral-weak">
+						© 2024 SasukeTech, <Link href=" https://raw.githubusercontent.com/SasukeGh/once-ui-design-for-nextjs/main/LICENSE?token=GHSAT0AAAAAACWGN46IT2W2G2F7DGWDK2LQZWW2Q6Q">MIT License</Link>
+					</Text>
+					<Flex
+						gap="12">
+						<Button
+							href="https://github.com/SasukeGh"
+							prefixIcon="github" size="s" variant="tertiary">
+							GitHub
+						</Button>
+						<Button
+							href="https://whatsapp.com/channel/0029Va8aNxzJENy5Obt7Tz0t"
+							prefixIcon="discord" size="s" variant="tertiary">
+							Whatsapp Channel
+						</Button>
+					</Flex>
 				</Flex>
 			</Flex>
-		</Flex>
-    </Analytics>
+			<Analytics /> {/* This line ensures the Analytics component is included */}
+		</React.Fragment>
 	);
 }
